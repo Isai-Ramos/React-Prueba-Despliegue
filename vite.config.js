f01f3da96220b7513960547db57f9a29b20644b4
Isai Ramos <isair@fmxfurniture.com>
2023-05-15 10:02:41 -0700

    configuracion de viteconfg

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/React-Prueba-Despliegue/'
+  base:'https://Isai-Ramos.github.io/React-Prueba-Despliegue/'
 })
